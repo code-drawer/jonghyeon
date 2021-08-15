ae4d29a740916ae467dd26c557fc8cc10d7b5caa
종현 박 <paper417@catholic.ac.kr>
2021-08-15 18:47:12 +0900

    static문 대신 extern문을 적용.

--- week14/week14/student.h
+++ week14/week14/student.h
@@ -5,11 +5,25 @@ using std::cout;
 using std::endl;
 using std::string;
 
+enum 권한 { 관리자 = 1, 외부인 };
+/*main파일에서 권한을 따로 초기화해두지 않는 한,
+권한은 기본적으로 null == 0 == false이다.
+관리자가 0이 되면 0 == false이므로
+권한 == 관리자 이다.
+이것은 맴버함수의 조건문을 true로 만들기 때문에 오류가 난다.
+이것을 방지하고자 관리자에 0이 아닌 1을 할당하였다.*/
+
+extern int 권한;
+/*extern문을 사용하여 다른 파일에서
+외부변수로써 가져다 쓸 수 있도록 한다고 선언한다.
+만약 static문을 사용한다면
+전역변수로써는 작동하지만
+다른 파일에서는 변수를 사용할 수가 없게된다.*/
+
 class student{
 	string 이름;
 	string 성별;
 	string 비번;
-	string 권한 = "외부인"; //기본값
 
 public:
 	student(string, string, string);
@@ -17,6 +31,4 @@ public:
 	void 이름바꿔(string);
 	string 비번줘();
 	void 비번바꿔(string);
-	string 권한줘();
-	void 권한바꿔();
 };
