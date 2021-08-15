69d3a7ad0193fd90f24a6f56595a70ced08f28b8
종현 박 <paper417@catholic.ac.kr>
2021-08-15 21:24:46 +0900

    .

--- week14/week14/전역변수 테스트.cpp
+++ week14/week14/전역변수 테스트.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int a; //전역변수는 초기화하지 않아도 기본값인 0으로 초기화된다.
+int x; //전역변수는 초기화하지 않아도 기본값인 0으로 초기화된다.
 
 int main() {
-	if (a == 0)
+	if (x == 0)
 		cout << 1 << endl;
-	if (a == false)
+	if (x == false)
 		cout << 2 << endl;
-	if (a == NULL)
+	if (x == NULL)
 		cout << 3 << endl;
 
 	return 0;
